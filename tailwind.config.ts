8a37a5bdc7cb441f1c949d2bdeabd93f64b3cfed
Albonire <andersonfabygonzalez.21@gmail.com>
2025-08-02 22:37:13 -0500

    fix: update Tailwind CSS configuration to use named import for tailwindcss-animate plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
-export default {
+const config = {
 	darkMode: ["class"],
 	content: [
 		"./pages/**/*.{ts,tsx}",
@@ -199,5 +200,7 @@ export default {
 			}
 		}
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [tailwindcssAnimate],
 } satisfies Config;
+
+export default config;
